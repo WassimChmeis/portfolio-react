e383ce6611c706fe83b99dca1f320ac9d288a186
Wassim Chmeis <wassimchmeis@gmail.com>
2023-05-25 18:17:52 +0300

    updating index.js changing ReactDom to root

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-// root.render(<App tab="home" />);
-ReactDOM.render(<App />, container);
+root.render(<App tab="home" />);
